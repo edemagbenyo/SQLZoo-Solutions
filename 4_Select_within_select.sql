641e482bcc886902b8eee4b99d5fef9a955c3324
Edem Agbenyo <edem.agbenyo@gmail.com>
2019-11-13 21:38:10 +0000

    Update select_within_select with 9, 10

--- 4_Select_within_select.sql
+++ 4_Select_within_select.sql
@@ -38,4 +38,12 @@ SELECT continent, name, area FROM world x
 SELECT DISTINCT(continent) cont,(SELECT name from world WHERE continent=cont LIMIT 1) from world 
 
 -- 9. Find the continents where all countries have a population <= 25000000. Then find the names of the countries associated with these continents. Show name, continent and population.
-
+SELECT name, continent, population FROM world 
+  WHERE continent IN (SELECT continent FROM world  x 
+    WHERE 25000000 >= (SELECT MAX(population) FROM world y 
+      WHERE x.continent = y.continent));
+                       
+-- 10. Some countries have populations more than three times that of any of their neighbours (in the same continent). Give the countries and continents.
+SELECT name, continent from world
+  WHERE population > ALL (SELECT population * 3 from world wd WHERE world.continent=wd.continent and world.name!=wd.name)                     
+                       
